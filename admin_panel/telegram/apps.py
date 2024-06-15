52ad56da77c1a60544d7e685bc2cde4cb8d46e65
Константин Стеблев <konstantin.sks85@gmail.com>
2024-06-15 14:41:01 +0200

    def create_default_entries

--- admin_panel/telegram/apps.py
+++ admin_panel/telegram/apps.py
@@ -5,3 +5,6 @@ class DjangoDBConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'admin_panel.telegram'
     verbose_name = 'админ панель'
+
+    # def ready(self):
+    #     import admin_panel.telegram.signals
