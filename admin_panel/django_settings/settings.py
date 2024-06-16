904c0708dd72fcb641d58f4e958e785646bc7e7e
Anna Pobedonostseva <115149921+ZebraHr@users.noreply.github.com>
2024-06-16 09:06:03 +0300

    fixes

--- admin_panel/django_settings/settings.py
+++ admin_panel/django_settings/settings.py
@@ -31,10 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY", "django-settings-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = [
-    env.str("HOST_IP"),
-    "bc2b-90-154-72-111.ngrok-free.app",
-]
+ALLOWED_HOSTS = [env.str('HOST_IP'),'localhost', '127.0.0.1',]
 
 # Application definition
 
